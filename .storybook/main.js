207bae438241a0db9d91d5018c31750c489cbf69
Lam <daibanglam@gmail.com>
2021-06-19 10:45:07 +0700

    refactor: remove css module support

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,29 +10,6 @@ module.exports = {
     "@storybook/addon-essentials"
   ],
   webpackFinal: (config) => {
-    // First we prevent webpack from using Storybook CSS rules to process CSS modules
-    config.module.rules.find(
-      rule => rule.test.toString() === '/\\.css$/'
-    ).exclude = /\.module\.css$/;
-
-    // Then we tell webpack what to do with CSS modules
-    config.module.rules.push({
-      test: /\.module\.css$/,
-      include: path.resolve(__dirname, '../src'),
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
-            modules: {
-              localIdentName: '[local]-[hash:base64:5]',
-            },
-          },
-        },
-      ],
-    });
-
     // use typescript loader for ts(x) file
     const idx = config.module.rules.findIndex(
       rule => rule.test.toString() === '/\\.(mjs|tsx?|jsx?)$/'
