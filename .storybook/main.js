a55774b01a00b99749795b01225375655e2fcd64
Lam <daibanglam@gmail.com>
2021-06-29 22:04:51 +0700

    fix: storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,30 +9,4 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ],
-  webpackFinal: (config) => {
-    // use typescript loader for ts(x) file
-    const idx = config.module.rules.findIndex(
-      rule => rule.test.toString() === '/\\.(mjs|tsx?|jsx?)$/'
-    );
-    if (idx !== -1) {
-      config.module.rules[idx] = {
-        test: /\.(t|j)s(x?)$/,
-        exclude: /node_modules/,
-        use: [
-          'ts-loader',
-          {
-            loader: 'react-docgen-typescript-loader',
-            options: {
-              // Provide the path to your tsconfig.json so that your stories can
-              // display types from outside each individual story.
-              tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
-            },
-          },
-        ],
-      };
-    }
-
-    // Return the altered config
-    return config;
-  },
 }
