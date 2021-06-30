6279807f17889076b57a3c4e49d7704d436eadb6
Lam <daibanglam@gmail.com>
2021-06-30 12:12:58 +0700

    feat: persist identity to localStorage

--- src/identity/atoms.ts
+++ src/identity/atoms.ts
@@ -1,7 +1,24 @@
-import { atom } from 'recoil';
+import { atom, AtomEffect } from 'recoil';
 import { Identity } from './types';
+
+const localStorageEffect: AtomEffect<Identity | undefined> = ({ setSelf, onSet }) => {
+  const key = 'identity';
+  const savedValue = localStorage.getItem(key);
+  if (savedValue != null) {
+    setSelf(JSON.parse(savedValue));
+  }
+
+  onSet((newValue) => {
+    if (newValue === undefined) {
+      localStorage.removeItem(key);
+    } else {
+      localStorage.setItem(key, JSON.stringify(newValue));
+    }
+  });
+};
 
 export const identityState = atom<Identity | undefined>({
   key: 'identityState', // unique ID (with respect to other atoms/selectors)
   default: undefined, // default value (aka initial value)
+  effects_UNSTABLE: [localStorageEffect],
 });
