4f231d0cc0bd039ccc1700d51e4b8e896df71299
Lam <daibanglam@gmail.com>
2022-03-30 21:43:56 +0700

    feat: update App component

---
+++ src/App/index.tsx
@@ -0,0 +1,63 @@
+import React, { Suspense } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DateAdapter from '@mui/lab/AdapterDateFns';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { SnackbarProvider } from 'notistack';
+
+import '../../../styles.css';
+import { theme } from '../theme';
+import routes from '../routes';
+import { ProtectedRoute } from '../identity';
+import LoadingFallback from '../common/atoms/LoadingFallback';
+import NotFoundPage from '../common/pages/NotFoundPage';
+import { ApiProvider } from '../api';
+import { API_BASE_URL } from '../config';
+import { ConfirmProvider } from '../confirm';
+
+const App: React.FC = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <LocalizationProvider dateAdapter={DateAdapter}>
+        <RecoilRoot>
+          <SnackbarProvider
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+          >
+            <ApiProvider baseUrl={API_BASE_URL}>
+              <ConfirmProvider>
+                <Router key={Math.random()}>
+                  <Suspense fallback={<LoadingFallback />}>
+                    <Switch>
+                      {routes.map((r) => (
+                        // Added property`key` to Router to fix warning
+                        // when hot reloading Route component
+                        <ProtectedRoute
+                          key={r.path}
+                          path={r.path}
+                          component={r.component}
+                          permissions={r.permissions}
+                          exact
+                        />
+                      ))}
+
+                      {/* 404 homepage */}
+                      <Route>
+                        <NotFoundPage />
+                      </Route>
+                    </Switch>
+                  </Suspense>
+                </Router>
+              </ConfirmProvider>
+            </ApiProvider>
+          </SnackbarProvider>
+        </RecoilRoot>
+      </LocalizationProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
