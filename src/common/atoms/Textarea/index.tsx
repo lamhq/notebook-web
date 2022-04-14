4bab9bc9539fe38f5ae641c08c1b3ac22e6a574b
Lam <daibanglam@gmail.com>
2022-04-14 20:50:34 +0700

    fix: eslint of TextArea

--- src/common/atoms/Textarea/index.tsx
+++ src/common/atoms/Textarea/index.tsx
@@ -9,12 +9,12 @@ interface TextareaProps extends TextareaAutosizeProps {
  * Customized TextareaAutosize to be used with TextField
  * https://material-ui.com/components/text-fields/#integration-with-3rd-party-input-libraries
  */
-const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  function Textarea(props, ref) {
-    const { inputRef = undefined, ...other } = props;
-    return <TextareaAutosize {...other} ref={ref} />;
-  }
-)
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(function Textarea(
+  props,
+  ref,
+) {
+  return <TextareaAutosize {...props} ref={ref} />;
+});
 
 Textarea.defaultProps = {
   inputRef: undefined,
