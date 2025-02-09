86bab8e81c54602a6b0f5304dbe56f4d832b617b
Lam <daibanglam@gmail.com>
2025-02-09 10:06:25 +0700

    chore: move favicon to `src` directory

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [pluginReact()],
   html: {
     title: 'Notebook',
-    favicon: 'public/favicon.svg',
+    favicon: 'src/favicon.svg',
   },
   server: {
     proxy: {
