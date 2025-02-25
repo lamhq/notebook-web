8f23aaa0b22b1936a73356c1fe8deda6f95990fb
Lam Huynh <daibanglam@gmail.com>
2025-02-25 14:06:04 +0700

    fix: missing env variables on build (#10)

--- infra/cloudfront.tf
+++ infra/cloudfront.tf
@@ -66,6 +66,7 @@ resource "aws_cloudfront_distribution" "web_distribution" {
     compress               = true
     viewer_protocol_policy = "allow-all"
     cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6" # CachingOptimized
+    # cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # CachingDisabled
     # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html#managed-cache-caching-optimized
 
     function_association {
